17d9408cc25fdcae22622dd05df60759ffa32dea
Samariddin <gotenks.ssj499@gmail.com>
2023-02-21 20:32:04 -0500

    todays class code

---
+++ 20230221/src/hooks/useLocalStorage.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+
+
+const useLocalStorage=(key, initialValue)=>{
+const [value, setValue]= useState(localStorage.getItem(key)||initialValue)
+
+useEffect(()=>{
+    localStorage.setItem(key, value)
+}, [value])
+
+
+const clear = () => {
+    setValue('');
+
+}
+
+return [
+    value,
+    setValue,
+    clear
+]
+}
+
+export default useLocalStorage
