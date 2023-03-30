4f490a499bc56cd9cb3ec38fe74941b33a49df7c
Samariddin <gotenks.ssj499@gmail.com>
2023-03-30 11:11:15 -0400

    march 29 class code

--- task4/src/components/SearchBar.jsx
+++ task4/src/components/SearchBar.jsx
@@ -1,29 +1,47 @@
-const SearchBar = ({search, setSearch, maxLength, setMaxLength, direction, setDirection}) => {
-    
-    const searchHandler = (e) => {
-        setSearch(e.target.value);
-    }
+const SearchBar = ({
+  search,
+  setSearch,
+  maxLength,
+  setMaxLength,
+  direction,
+  setDirection,
+}) => {
+  const searchHandler = (e) => {
+    setSearch(e.target.value);
+  };
 
-    const maxLengthHandler = (e) => {
-        setMaxLength(e.target.value);
-    }
+  const maxLengthHandler = (e) => {
+    setMaxLength(e.target.value);
+  };
 
-    const sortDirectionHandler=()=>{
-        setDirection("");
-    }
+  const sortDirectionHandler = () => {
+    setDirection("asc");
+  };
 
-    return <>
-        <label htmlFor="search">Search Query</label>
-        <input type="text" name="search" onChange={searchHandler} value={search} />
-        <label htmlFor="max-length">Max Length</label>
-        <input type="number" name="max-length" onChange={maxLengthHandler} value={maxLength} />
-        <button name="sort-direction" onClick={sortDirectionHandler} >{direction==='asc' ? 'Sort Descending':'Sort Ascending'}</button>
+  return (
+    <>
+      <label htmlFor="search">Search Query</label>
+      <input
+        type="text"
+        name="search"
+        onChange={searchHandler}
+        value={search}
+      />
+      <label htmlFor="max-length">Max Length</label>
+      <input
+        type="number"
+        name="max-length"
+        onChange={maxLengthHandler}
+        value={maxLength}
+      />
+      <button name="sort-direction" onClick={sortDirectionHandler}>
+        {direction === "asc" ? "Sort Descending" : "Sort Ascending"}
+      </button>
     </>
-}
+  );
+};
 
 export default SearchBar;
-
-
 
 // const sorted Movies= [...movies]
 // if (direction ==='asc') {
